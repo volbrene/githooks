74b331c070bf08fb524e261f1b1ce90136a59b62
Rene Volbach <rene@volbach.eu>
2025-09-16 18:17:40 +0200

    feat: enhance project setup with semantic release and CI/CD

---
+++ release.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  branches: ['main'],
+  preset: 'conventionalcommits',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/changelog', { changelogFile: 'CHANGELOG.md' }],
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+};
